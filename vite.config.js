00f3d1015032bc924d8de3d16c500ed979856a16
Abdulkafi Nirig <abdulkafinirig@Abdulkafis-MacBook-Pro.local>
2025-08-27 14:41:38 -0700

    Fix duplicate key, rebuild for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -14,3 +14,24 @@ export default defineConfig({
   base: '/HoopCast/',   
   plugins: [react()],
 })
+
+const gameDetails = {
+  season: game.season,
+  uid: game.uid,
+  userId,
+  docId
+};
+
+const transformedArray = arr.map(({ game, userId, docId }) => ({
+  name: game.name,
+  competitions: game.competitions,
+  links: game.links,
+  status: game.status,
+  shortName: game.shortName,
+  userId, // Keep only one userId
+  date: game.date,
+  id: game.id,
+  season: game.season,
+  uid: game.uid,
+  docId
+}));
