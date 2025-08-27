204dc5212b75a67a32b16d8d0cb0e3f6263fe7a4
Abdulkafi Nirig <abdulkafinirig@Abdulkafis-MacBook-Pro.local>
2025-08-27 14:52:00 -0700

    Fix routing for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { gameDetailsFrom, transformGames } from './src/utils/transform';
 
 export default defineConfig({
-  base: '/HoopCast/',
+  base: '/HoopCast/', // Ensure this matches your GitHub repository name
   plugins: [react()],
 });
 
-// Example usage of the utility functions
-const gameDetails = gameDetailsFrom(game, userId, docId);
+// filepath: /Users/abdulkafinirig/Documents/basketball-schedule/src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { HashRouter as Router, Route, Navigate } from 'react-router-dom';
 
-const transformedArray = transformGames(arr);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <Route path="/" element={<Navigate to="/Home" />} />
+      <App />
+    </Router>
+  </React.StrictMode>
+);
